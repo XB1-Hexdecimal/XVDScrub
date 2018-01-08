cbca0d8705dbdfc16e4cc20af4cb0c1630c01ea4
HostyOS <XB1-Hexdecimal@users.noreply.github.com>
2018-01-08 02:20:48 -0600

    added comments to offsets

--- xvdstrip.py
+++ xvdstrip.py
@@ -3,11 +3,11 @@ import sys
 if len(sys.argv) > 1:
     xvdpath = sys.argv[1]
     fh = open(sys.argv[1], "r+b")
-    fh.seek(0x896)
+    fh.seek(0x896) #SerialOffset
     zerobytes = "00000000000000".encode()
     zerobytes2 = "X000000-000".encode()
     fh.write(zerobytes)
-    fh.seek(0x8CC)
+    fh.seek(0x8CC) #ODDModelOffset
     fh.write(zerobytes2)
     fh.close()
     print("Identifiable info was overwritten. ")
